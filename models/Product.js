3c4cb005e4fef3f22f55b80ece07fcf88639484f
andytheelf <ballew22@gmail.com>
2020-06-13 16:05:13 -0500

    models update

--- models/Product.js
+++ models/Product.js
@@ -7,17 +7,36 @@ const sequelize = require('../config/connection');
 class Product extends Model {}
 
 // set up fields and rules for Product model
-Product.init(
-  {
+Product.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
     // define columns
-  },
-  {
+    product_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    price: {
+        allowNull: false,
+        type: DataTypes.INTEGER
+    },
+    stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    category_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+
+    }
+}, {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'product',
-  }
-);
+});
 
 module.exports = Product;
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -4,17 +4,27 @@ const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
-ProductTag.init(
-  {
+ProductTag.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
     // define columns
-  },
-  {
+    product_id: {
+        allowNull: false,
+        type: DataTypes.INTEGER
+    },
+    tag_id: {
+        allowNull: false,
+        type: DataTypes.INTEGER
+    }
+}, {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'product_tag',
-  }
-);
+});
 
 module.exports = ProductTag;
